1f637811b6b0930e89b6cb1a55bdb8e1193849ee
liujiage <liujiage@mozat.com>
2020-05-29 21:33:27 +0800

    for testing

--- org.jiage.srpc.server/src/test/java/org/jiage/srpc/server/list/ListOrderTest.java
+++ org.jiage.srpc.server/src/test/java/org/jiage/srpc/server/list/ListOrderTest.java
@@ -22,7 +22,58 @@ public class ListOrderTest {
         list.stream().forEach(TestOrder::print);
     }
 
+    @Test
+    public void testCopy(){
+        List<TestOrder> list = new ArrayList<>();
+        list.add(new TestOrder(11111111L,1113,1));
+        list.add(new TestOrder(11111112L,1113,2));
+        list.add(new TestOrder(11111113L,1113,3));
+        list = this.settingValue(list);
+        list.stream().forEach(TestOrder::print);
+        System.out.println(list.toString());
 
+    }
+
+    public List<TestOrder> settingValue(List<TestOrder> list){
+        list.get(0).setNumbers(9999);
+        list = list.stream().filter(v->v.getId()==0).collect(Collectors.toList());
+        System.out.println(list.toString());
+        return list;
+    }
+
+
+    @Test
+    public void testListCopy(){
+        List<TestOrder> list = new ArrayList<>();
+        list.add(new TestOrder(11111111L,1113,1));
+        list.add(new TestOrder(11111112L,1113,2));
+        list.add(new TestOrder(11111113L,1113,3));
+        TestList t = new TestList(list);
+        settingListValue(t);
+        System.out.println(t.getList());
+        System.out.println(list);
+    }
+
+    public void settingListValue(TestList testList){
+        testList.setList(new ArrayList<>());
+    }
+
+
+}
+class TestList{
+    private  List<TestOrder> list;
+
+    public TestList(List<TestOrder> list) {
+        this.list = list;
+    }
+
+    public List<TestOrder> getList() {
+        return list;
+    }
+
+    public void setList(List<TestOrder> list) {
+        this.list = list;
+    }
 }
 class TestOrder{
 
